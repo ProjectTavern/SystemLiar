edf4e72af0cc89ae1b907769ada96e8cf5e49503
qoov <emrys8128@gmail.com>
2018-09-22 18:23:52 +0900

    레디스 기본 설정 테스트

---
+++ references.md
@@ -0,0 +1,1 @@
+[Node-Redis 사용하기](https://askofcloud.wordpress.com/2016/04/05/node-%EC%97%90%EC%84%9C-redis-%EC%82%AC%EC%9A%A9%ED%95%98%EA%B8%B0/)
--- src/redis.js
+++ src/redis.js
@@ -1,8 +1,20 @@
 /**
  * 레디스 기본 설정 및 세팅
  * */
-const redis = require('redis');
-const client = redis.createClient();
+const Redis = require('ioredis');
+const JSON = require('JSON');
+
+const CONFIG = {
+  "SERVER" : {
+    port: 6379,
+    host: 'localhost',
+    password: 'password',
+    db: 0
+  },
+  "rankingBoard" : "rankingBoard"
+};
+
+client = new Redis(CONFIG.SERVER);
 
 const SYSTEMMESSAGE = {
   GETDATAERROR: "데이터를 가져올 수 없습니다."
@@ -16,10 +28,6 @@ client.get('name', (err, obj) => {
   if (err) throw new Error(SYSTEMMESSAGE.GETDATAERROR);
   console.log(obj);
 });
-
-const config = {
-  "ranking-board" : "ranking-board"
-};
 
 const testUserId1 = {
   "hashID" : "!@adf@ZD@$!!@#",
@@ -37,17 +45,17 @@ const testUserId3 = {
 /**
  * 목업데이터를 획득하여 초기 설정값 세팅
  * */
-client.zadd(config["ranking-board"], 100, JSON.stringify(testUserId1));
-client.zadd(config["ranking-board"], 200, JSON.stringify(testUserId2));
-client.zadd(config["ranking-board"], 300, JSON.stringify(testUserId3));
+client.zadd(CONFIG["rankingBoard"], 100, JSON.stringify(testUserId1));
+client.zadd(CONFIG["rankingBoard"], 200, JSON.stringify(testUserId2));
+client.zadd(CONFIG["rankingBoard"], 300, JSON.stringify(testUserId3));
 
 /**
  * 추가적으로 얻은 성적 데이터는 zincrby를 활용하여 값을 더해줌
  * */
-client.zincrby(config["ranking-board"], 100, JSON.stringify(testUserId1));
+client.zincrby(CONFIG["rankingBoard"], 100, JSON.stringify(testUserId1));
 
 /* 랭킹 데이터 출력 */
-client.zrevrange(config["ranking-board"], 0, 2, (err, obj) => {
+client.zrevrange(CONFIG["rankingBoard"], 0, 2, (err, obj) => {
   console.log(obj);
   const rankingData = obj.map(elem => JSON.parse(elem).nickname);
   console.log(rankingData);
