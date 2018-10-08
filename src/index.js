a1d775b973f4ab1b2b04ff3a0d2999b2accb0e32
qoov <emrys8128@gmail.com>
2018-10-08 22:48:15 +0900

    게임 시작

--- src/index.js
+++ src/index.js
@@ -317,7 +317,7 @@ roomspace.on('connection', socket => {
         console.log("[LOG][create:room] 방이 생성되었습니다.", roomData);
         /* 합쳐야할지 고민 */
         socket.join(data.id);
-        socket.userRooms.push(data.id);
+        socket.userRooms.push(roomId);
 
         setNameTag(socket, usersession.userinfo.nickname);
         socket.emit("create:room", true);
