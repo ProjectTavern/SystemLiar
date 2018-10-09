73f4440d2318f548746df68771a1e6f270689736
qoov <emrys8128@gmail.com>
2018-10-09 22:11:51 +0900

    모두 나간 방 제거하기

--- src/index.js
+++ src/index.js
@@ -483,7 +483,7 @@ roomspace.on('connection', socket => {
       initRoom(socket);
       roomspace.to(roomId).emit("system:message", { message: userNickname + '님이 방에서 나가셨습니다.' });
       if (selectedRoom.members.length === 0) {
-        console.log("[LOG][disconnect] 방에 아무도 없어 방을 삭제합니다.", rooms[data.number]);
+        console.log("[LOG][disconnect] 방에 아무도 없어 방을 삭제합니다.", selectedRoom);
         rooms.splice(rooms.indexOf(selectedRoom), 1);
       }
       console.log("[LOG][disconnect] 현재 방의 정보들", rooms);
