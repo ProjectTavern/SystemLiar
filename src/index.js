18e61bb5b0c338a7d7db1658dd76967c73893483
qoov <emrys8128@gmail.com>
2018-11-01 22:08:25 +0900

    [qoov] 로그 페이지

--- src/index.js
+++ src/index.js
@@ -285,34 +285,6 @@ roomspace.on('connection', socket => {
     }
   }
 
-  /**
-   * EmitJson의 사용으로 불필요해짐
-   * 유니티 모듈 변경이 필요해진 경우 다시 사용할 수도 있음
-   * */
-  function parseData(data) {
-    try {
-      data = typeof data === "string" ? JSON.parse(data) : data;
-    } catch(e) {
-      console.log("[ERROR] Can not parse to JSON from string object.");
-    }
-    return data;
-  }
-
-  /**
-   * 선택된 방을 찾음
-   * */
-  function getSelectedRoom(rooms, id) {
-    const checkRoom = rooms.filter(element => {
-      return element.id + "" === id + "";
-    });
-    console.log("check",checkRoom);
-    let selectedRoom = {};
-    if (checkRoom.length) {
-      selectedRoom = checkRoom[0];
-    }
-    return selectedRoom;
-  }
-
   socket.on('disconnect', () => {
     console.log("[LOG][disconnect] 유저의 연결이 끊어졌습니다.");
 
@@ -421,14 +393,12 @@ roomspace.on('connection', socket => {
     }
   });
 
-
-
   socket.emit("rooms:info", filterRooms(rooms));
 
   /* 토론의 종료 */
   socket.on('end:discuss', (data) => {
     const selectedRoom = getSelectedRoom(rooms, socket.userRooms[0]);
-    roomspace.to(socket.userRooms[0]).emit("vote:list", selectedRoom.currentUsers);
+    roomspace.to(socket.userRooms[0]).emit("vote:list", selectedRoom.currentUsers.map(userinfo => userinfo.nickname));
   });
 });
 
@@ -461,4 +431,32 @@ function filterRooms(rooms) {
 
 function deepCopy(data) {
   return JSON.parse(JSON.stringify(data));
+}
+
+/**
+ * 선택된 방을 찾음
+ * */
+function getSelectedRoom(rooms, id) {
+  const checkRoom = rooms.filter(element => {
+    return element.id + "" === id + "";
+  });
+  console.log("check",checkRoom);
+  let selectedRoom = {};
+  if (checkRoom.length) {
+    selectedRoom = checkRoom[0];
+  }
+  return selectedRoom;
+}
+
+/**
+ * EmitJson의 사용으로 불필요해짐
+ * 유니티 모듈 변경이 필요해진 경우 다시 사용할 수도 있음
+ * */
+function parseData(data) {
+  try {
+    data = typeof data === "string" ? JSON.parse(data) : data;
+  } catch(e) {
+    console.log("[ERROR] Can not parse to JSON from string object.");
+  }
+  return data;
 }
