d99973be91c6d81290207b52fffe005f3afe97f3
qoov <emrys8128@gmail.com>
2018-11-02 20:35:34 +0900

    [qoov] 구조 개선 시도

--- src/controllers/socketio/ChatProgramOn/index.js
+++ src/controllers/socketio/ChatProgramOn/index.js
@@ -2,7 +2,7 @@ const { logger, dataLogger } = require('../../../utilities/logger/winston');
 const redis = require('../../database/redis');
 let rooms = require('./rooms');
 
-module.exports = (socket) => {
+module.exports = function (socket) {
   logger.custLog(`사용자가 접속하였습니다. 해당 사용자의 아이디는 ${socket.id} 입니다. 소켓 접속에 사용자의 세션 정보를 불러옵니다.`);
 
   socket.userRooms = [];
