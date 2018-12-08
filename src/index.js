32ee3caac9c975b2583aecf359f51bd2101a259a
qoov <emrys8128@gmail.com>
2018-12-08 16:55:42 +0900

    [qoov] 방 번호

--- src/index.js
+++ src/index.js
@@ -521,6 +521,7 @@ function filterRooms(rooms) {
   return rooms.map(room => {
     return {
       id: room.id,
+      number: room.number,
       name: room.name,
       subject: room.subject,
       members: room.members,
