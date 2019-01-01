d0beb2a78d42de2aef88b827e628adfa508f8aea
qoov <emrys8128@gmail.com>
2019-01-02 00:19:44 +0900

    [qoov] 소켓셋 테스트

--- src/controllers/socketio/events/gameProcess/sendMessage.js
+++ src/controllers/socketio/events/gameProcess/sendMessage.js
@@ -1,11 +1,7 @@
 module.exports = function sendMessage(responseData) {
   const { socket, ChatSocketIO } = this;
   const userSession = socket.handshake.session;
-  try {
-    responseData.nickname = userSession.userinfo.nickname;
-    const roomId = userSession.userinfo.room;
-    ChatSocketIO.to(roomId).emit("user:message", responseData);
-  } catch (error) {
-
-  }
+  responseData.nickname = userSession.userinfo.nickname;
+  const roomId = userSession.userinfo.room;
+  ChatSocketIO.to(roomId).emit("user:message", responseData);
 };
