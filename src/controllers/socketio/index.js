486c5837e80fea6ea59c8bf61952fdcaf46894c7
sieeid <sieeid8128@gmail.com>
2019-01-11 22:33:12 +0900

    변수 상위로 수정

--- src/controllers/socketio/index.js
+++ src/controllers/socketio/index.js
@@ -3,50 +3,57 @@ const users = require('../../../database/Users');
 
 const rooms = require('./rooms');
 
+// 로그인
+const userStatus = require('./events/userInformation/userStatus');
+const userCreateNickname = require('./events/userInformation/userCreateNickname');
+// 게임방
+const roomsRefresh = require('./events/roomProcess/refreshRoom');
+const createRoom = require('./events/roomProcess/createRoom');
+const joinRoom = require('./events/roomProcess/joinRoom');
+const getSubject = require('./events/roomProcess/getSubject');
+const leaveRoom = require('./events/roomProcess/leaveRoom');
+// 인게임
+const sendMessage = require('./events/gameProcess/sendMessage');
+const readyUser = require('./events/gameProcess/readyUser');
+const startGame = require('./events/gameProcess/startGame');
+const explainGame = require('./events/gameProcess/explainGame');
+const endDiscuss = require('./events/gameProcess/endDiscuss');
+const voteGame = require('./events/gameProcess/voteGame');
+const lastChance = require('./events/gameProcess/lastChance');
+const lastAnswer = require('./events/gameProcess/lastAnswer');
+const endGame = require('./events/gameProcess/endGame');
+// 접속종료
+const disconnect = require('./events/userInformation/disconnect');
+
 module.exports = function bindEventChatSocket(ChatSocketIO) {
   ChatSocketIO.on('connection', socket => {
     users.connected();
     const socketSet = { socket: socket, ChatSocketIO: ChatSocketIO };
 
     // 로그인
-    const userStatus = require('./events/userInformation/userStatus');
     socket.on('user:status', userStatus.bind(socketSet));
-    const userCreateNickname = require('./events/userInformation/userCreateNickname');
     socket.on('user:create:nickname', userCreateNickname.bind(socketSet));
 
     // 게임방
     socket.emit('rooms:info', filterRooms(rooms));
-    const roomsRefresh = require('./events/roomProcess/refreshRoom');
     socket.on('rooms:refresh', roomsRefresh.bind(socketSet));
-    const createRoom = require('./events/roomProcess/createRoom');
     socket.on('create:room', createRoom.bind(socketSet));
-    const joinRoom = require('./events/roomProcess/joinRoom');
     socket.on('join:room', joinRoom.bind(socketSet));
-    const getSubject = require('./events/roomProcess/getSubject');
     socket.on('get:subject', getSubject.bind(socketSet));
-    const leaveRoom = require('./events/roomProcess/leaveRoom');
     socket.on('leave:room', leaveRoom.bind(socketSet));
 
     // 인게임
-    const sendMessage = require('./events/gameProcess/sendMessage');
     socket.on('send:message', sendMessage.bind(socketSet));
-    const readyUser = require('./events/gameProcess/readyUser');
     socket.on('ready:user', readyUser.bind(socketSet));
-    const startGame = require('./events/gameProcess/startGame');
     socket.on('start:game', startGame.bind(socketSet));
-    const explainGame = require('./events/gameProcess/explainGame');
     socket.on('explain:game', explainGame.bind(socketSet));
-    const endDiscuss = require('./events/gameProcess/endDiscuss');
     socket.on('end:discuss', endDiscuss.bind(socketSet));
-    const voteGame = require('./events/gameProcess/voteGame');
     socket.on('vote:game', voteGame.bind(socketSet));
-    const lastChance = require('./events/gameProcess/lastChance');
     socket.on('last:chance', lastChance.bind(socketSet));
-    const lastAnswer = require('./events/gameProcess/lastAnswer');
     socket.on('last:answer', lastAnswer.bind(socketSet));
-    const endGame = require('./events/gameProcess/endGame');
     socket.on('end:game', endGame.bind(socketSet));
-    const disconnect = require('./events/userInformation/disconnect');
+
+    // 접속종료
     socket.on('disconnect', disconnect.bind(socketSet));
   });
 };
