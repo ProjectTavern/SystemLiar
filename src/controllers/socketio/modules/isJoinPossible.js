967d195483c99599705ec1db0e922f6d29fed10c
sieeid <sieeid8128@gmail.com>
2019-01-12 17:35:38 +0900

    입장가능

--- src/controllers/socketio/modules/isJoinPossible.js
+++ src/controllers/socketio/modules/isJoinPossible.js
@@ -1,6 +1,6 @@
 module.exports = function isJoinPossible(selectedRoom, nickname) {
   const isNotJoined = !((selectedRoom.members.filter(element => element === nickname)).length);
   return selectedRoom.status === "대기중"
-    && selectedRoom.members.length < selectedRoom.limit
+    && selectedRoom.members.filter(member => member).length < selectedRoom.limit
     && isNotJoined;
 };
